fff47ddbeb01a88609f1a3ba6b9844ac2a683922
henryglazt <34293559+henryglazt@users.noreply.github.com>
2020-12-13 16:06:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://anya-priv8.glitch.me"]
+const urls = ["ANJING-ANJING-ANJING-ANJING"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
